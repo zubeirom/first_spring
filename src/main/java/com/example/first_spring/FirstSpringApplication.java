2bcfa9f8ac12e02a93edc4174d94bc3ce6d9694e
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-06-18 23:30:49 +0200

    run spring and print something in console

--- src/main/java/com/example/first_spring/FirstSpringApplication.java
+++ src/main/java/com/example/first_spring/FirstSpringApplication.java
@@ -8,6 +8,9 @@ public class FirstSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FirstSpringApplication.class, args);
+
+        System.out.println("Welcome to Spring Boot");
+
     }
 
 }
